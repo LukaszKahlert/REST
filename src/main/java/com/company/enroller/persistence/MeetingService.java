4e6166460b92250474c63bd44aaaebe6f85eedfa
LukaszKahlert <lukasz.kahlert@gmail.com>
2018-05-17 22:11:33 +0200

    commit

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -18,9 +18,21 @@ public class MeetingService {
 	}
 
 	public Collection<Meeting> getAll() {
-		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
-		return query.list();
+//		String hql = "FROM Meeting";
+//		Query query = connector.getSession().createQuery(hql);
+//		return query.list();
+		return connector.getSession().createCriteria(Meeting.class).list();
+	}
+	
+	public Meeting findById(long l) {
+		return (Meeting) connector.getSession().get(Meeting.class, l);
+
+	}
+
+	public void addMeeting(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
 	}
 
 }
